d946d45ae44377e1df180e91df015eb50fabd480
jojofragoso <joelitonbfragoso@gmail.com>
2022-03-01 20:37:05 -0300

    adicionando explicação sobre as operações

--- operadoresAritimericos.php
+++ operadoresAritimericos.php
@@ -5,12 +5,13 @@ echo "www.operadoresAritimericos.com.br<br/><br/>";
 $a = 16;
 $b = 3;
 
-$c = $a + $b;
-$c = $a - $b; 
-$c = $a + $b;
-$c = $a / $b; 
-$c = $a * $b;
-$c = $a % $b;
+$c = $a + $b; echo "Operação da Linha ".__LINE__." traz  O resultado da soma de: ".$a." + ".$b." que é igual a: ".$c."<br/>";
+$c = $a - $b; echo "Operação da Linha ".__LINE__." traz  O resultado da subtração de: ".$a." - ".$b." que é igual a: ".$c."<br/>";
+$c = $a + $b; echo "Operação da Linha ".__LINE__." traz  O resultado da soma de: ".$a." + ".$b." que é igual a: ".$c."<br/>";
+$c = $a / $b; echo "Operação da Linha ".__LINE__." traz  O resultado da divisão de: ".$a." / ".$b." que é igual a: ".$c."<br/>";
+$c = $a * $b; echo "Operação da Linha ".__LINE__." traz  O resultado da multiplicação de: ".$a." * ".$b." que é igual a: ".$c."<br/>";
+$c = $a % $b; echo "Operação da Linha ".__LINE__." traz  O resultado do resto da divisão de: ".$a." por ".$b." que é igual a: ".$c."<br/>"; 
+
  
 echo $c;
 
