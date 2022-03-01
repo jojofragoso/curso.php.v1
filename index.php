74de563a7b101594fab828f98eff925601f09344
jojofragoso <joelitonbfragoso@gmail.com>
2022-03-01 20:38:58 -0300

    operadoresDeAtribuicao.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <ul>
     <li> <a href="conceitosDeVariaveis.php"> Aula 01 </a> </li>
-    <li> <a href="operadoresAritimericos.php"> Aula 01 </a> </li>
-    <li> <a href="operadoresDeArAtribuicao"> Aula 01 </a> </li>
+    <li> <a href="operadoresAritimericos.php"> Aula 02 </a> </li>
+    <li> <a href="operadoresDeAtribuicao.php"> Aula 03 </a> </li>
 
 </ul>
